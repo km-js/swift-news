83e8a84c8ea0ea3e6444324491ba011bbc639c22
Kopal Mehta <kopal.mehta22@gmail.com>
2023-12-17 06:20:00 +0530

    deleted extra files

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,9 @@
-
-//import { useEffect, useState } from 'react';
 import fetchNews from '../utils/fetchNews';
 import Hero from './components/Hero';
 import NewsList from './components/NewsList';
 import { apiUrl } from '@/utils/constants';
 
 async function Home() {
- // const [news, setNews] = useState([]);
-
-  // useEffect(() => {
-  //   const getNews = async () => {
-  //     try {
-  //       let newsData = await fetchNews(apiUrl);
-  //       console.log(apiUrl)
-  //       console.log(newsData)
-  //       setNews(newsData);
-  //     }
-  //     catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
-  //   getNews();
-  // }, [])
-
   const news = await fetchNews(apiUrl)
 
   return (
