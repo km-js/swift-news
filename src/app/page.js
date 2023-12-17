43a2b2b7af5765cb3eca5a6311f2f9b93e1acef5
Kopal Mehta <kopal.mehta22@gmail.com>
2023-12-17 06:18:37 +0530

    changed page from client component to server component

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
-'use client'
-import { useEffect, useState } from 'react';
+
+//import { useEffect, useState } from 'react';
 import fetchNews from '../utils/fetchNews';
 import Hero from './components/Hero';
 import NewsList from './components/NewsList';
 import { apiUrl } from '@/utils/constants';
 
-function Home() {
-  const [news, setNews] = useState([]);
+async function Home() {
+ // const [news, setNews] = useState([]);
 
-  useEffect(() => {
-    const getNews = async () => {
-      try {
-        let newsData = await fetchNews(apiUrl);
-        console.log(apiUrl)
-        console.log(newsData)
-        setNews(newsData);
-      }
-      catch (error) {
-        console.error(error)
-      }
-    }
-    getNews();
-  }, [])
+  // useEffect(() => {
+  //   const getNews = async () => {
+  //     try {
+  //       let newsData = await fetchNews(apiUrl);
+  //       console.log(apiUrl)
+  //       console.log(newsData)
+  //       setNews(newsData);
+  //     }
+  //     catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+  //   getNews();
+  // }, [])
+
+  const news = await fetchNews(apiUrl)
 
   return (
     <main>
